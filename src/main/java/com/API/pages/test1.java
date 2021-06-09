a3f22fa4cfc5cdb77baa204806d1babd575cff2c
diwakar kumar <diwisuji@gmail.com>
2021-06-09 18:12:19 +0530

    this is after revert

--- src/main/java/com/API/pages/test1.java
+++ src/main/java/com/API/pages/test1.java
@@ -3,10 +3,9 @@ package com.API.pages;
 public class test1 {
 	
 	public static void main(String[] args) {
-		for(int i:10) {
+		for(int i=0;i<10;i++) {
 			System.out.println(i);
 		}
-		int k=0;
 	}
 	
 }
